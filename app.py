fc11474e4c2401fbc0b97115c54d159bdb497f8d
Aryan Gupta <148433753+AryanGupta825@users.noreply.github.com>
2026-06-08 16:14:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -162,5 +162,5 @@ def chat_history(job_id):
 
 
 if __name__ == "__main__":
-    port = int(os.getenv("PORT", 10000))
+    port = int(os.getenv("PORT", 7860))
     app.run(debug=False, host="0.0.0.0", port=port)
